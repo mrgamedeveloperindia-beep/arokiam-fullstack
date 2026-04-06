90f6536906a2f2c94ad2834d194d07c117637c08
mrgamedeveloperindia-beep <mrgamedeveloperindia@gmail.com>
2026-04-06 12:40:59 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,23 @@
-
 'use client';
 import { useEffect, useState } from 'react';
-import Head
+
+export default function Home() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    fetch('/landing.html')
+      .then(res => res.text())
+      .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        setContent(doc.body.innerHTML);
+      });
+  }, []);
+
+  return (
+    <div 
+      className="min-h-screen"
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  );
+}
